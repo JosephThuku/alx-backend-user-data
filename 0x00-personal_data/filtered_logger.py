4bd65756acaa0bfcfa56137664732974a7921660
JosephThuku <jthuku490@gmail.com>
2023-08-03 10:00:13 +0300

    reges

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env pthon3
-"""Regex-ing"""
+"""
+Regex-ing
+"""
 import re
 from typing import List
 import logging
@@ -8,7 +10,9 @@ import os
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
+    """
+    returns the log message obfuscated
+    """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
