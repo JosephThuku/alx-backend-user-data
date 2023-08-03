291f619f4bc968998ade766bfc928b5fac21392b
JosephThuku <jthuku490@gmail.com>
2023-08-03 09:51:40 +0300

    task one

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pthon3
+"""Regex-ing"""
+import re
+from typing import List
+import logging
+import os
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
